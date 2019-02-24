eacd25919cf78478716141b446dc381145ae6024
Kristen Z <zermattenk@gmail.com>
2019-02-24 16:06:14 -0600

    Removed Routes and AddItem

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Contentbox from 'Contentbox';
+import Contentbox from './Contentbox';
 
 class App extends Component {
   state= {
--- src/containers/Contentbox.js
+++ src/containers/Contentbox.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Calculator from 'Calculator';
-import NewItem from 'NewItem';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class Contentbox extends Component {
   render() {
@@ -9,12 +7,7 @@ class Contentbox extends Component {
       <div className="contentbox-wrapper">
         <h1>Carb Calculator</h1>
         <div className="contentbox">
-        <Router>
-          <Switch>
-            <Route path="/" component={Calculator}/>
-            <Route path="/newitem" component={NewItem}/>
-          </Switch>
-        </Router>
+          <Calculator />
         </div>
       </div>
     )
