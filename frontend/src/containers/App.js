a3d921590b95d116d4040b798de5c4bfc0926204
Kristen Z <zermattenk@gmail.com>
2019-02-28 16:39:47 -0600

    Moved Navbar into ContentBox and restyled

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 
 import Contentbox from '../containers/Contentbox';
 import Navbar from '../components/Navbar';
 
+
 class App extends Component {
-  state= {
-    users: []
-  };
-
-  // componentDidMount() {
-  //   fetch('/users')
-  //     .then(res => res.json())
-  //     .then(users => this.setState({ users }));
-  // }
-
   render() {
     return (
       <div id="background">
--- frontend/src/containers/Contentbox.js
+++ frontend/src/containers/Contentbox.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import Calculator from '../components/Calculator';
 import ItemsList from '../components/ItemsList';
 import NewItem from '../components/NewItem';
 import EditItem from '../components/EditItem';
+import Navbar from '../components/Navbar';
 
 class Contentbox extends Component {
   render() {
     return (
-      <div className="contentbox-wrapper">
-        <h1>Carb Calculator</h1>
-        <div className="contentbox">
+      <div className='contentbox-wrapper'>
         <Router>
-          <Switch>
-            <Route exact path='/' component={Calculator} />
-            <Route path='/all-items' component={ItemsList} />
-            <Route path='/new-item' component={NewItem} />
-            <Route path='/edit/>:id/' component={EditItem} />
-          </Switch>
+          <div>
+            <div id='title-nav'>
+              <h1>Carb Calculator</h1>
+              <Navbar />
+            </div>
+            <div className='contentbox'>
+                <Switch>
+                  <Route exact path='/' component={Calculator} />
+                  <Route path='/all-items' component={ItemsList} />
+                  <Route path='/new-item' component={NewItem} />
+                  <Route path='/edit/>:id/' component={EditItem} />
+                </Switch>
+              </div>
+          </div>
         </Router>
-        </div>
       </div>
     )
   }
