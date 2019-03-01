a251b6c9512627bad7fec422865d6803f4f4723c
Kristen Z <zermattenk@gmail.com>
2019-03-01 16:55:32 -0600

    Created MiniCalculator, added to Menu

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ItemService from './ItemService';
 
+import MiniCalculator from '../components/MiniCalculator';
+
 class Menu extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-
+            carbsDesired: 0
         }
     }
 
@@ -14,6 +16,13 @@ class Menu extends Component {
         return (
             <div className='content-container'>
                 <h2 className='content-header'>Menu</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <div className='form-row'>
+                        <label htmlFor='carbsDesired'>Total Carbs Desired&#58;</label>
+                        <input type='number' id='carbsDesired' name='carbsDesired' onChange={this.handleChange} />
+                    </div>
+                </form>
+                <MiniCalculator />
             </div>
         )
     }
