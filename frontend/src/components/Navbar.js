22a8a30cfa66c7dcb1d097a0db37b65e766ba75f
Kristen Z <zermattenk@gmail.com>
2019-03-11 10:19:26 -0500

    Started vertical navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,10 +6,10 @@ class Navbar extends Component {
     return (
         <div className="nav-container">
           <ul>
+            <li><Link to="/">Calculator</Link></li>
             <li><Link to="/new-item">New Item</Link></li>
             <li><Link to="/all-items">Items</Link></li>
             <li><Link to="/menu">Menu</Link></li>
-            <li><Link to="/">Calculator</Link></li>
           </ul>
         </div>
     )
